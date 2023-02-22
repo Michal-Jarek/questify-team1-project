15b9edbadbe937ad67337ad1bc0ba9871f466e79
piotrekzd <piotrek.zdebski@gmail.com>
2023-02-22 01:15:13 +0100

    starting work on rendering cards in groups

---
+++ src/components/CardGroup/CardGroupContainer.jsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import { useSelector } from 'react-redux';
+// import { CardGroup } from './CardGroup';
+
+// export const CardGroupContainer = () => {
+
+// return (
+//     <>
+//         <CardGroup groupName='past' />
+//         <CardGroup groupName='today' />
+//         <CardGroup groupName='tomorrow' />
+//         <CardGroup groupName='upcoming' />
+//     </>
+// );
+    
+// };
