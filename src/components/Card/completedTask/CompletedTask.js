e131ed6a594bb2bb9fbd819e2ff0d296adc78f22
Daria Ośka-Ossowska <daria.oska@gmail.com>
2023-02-27 13:53:30 +0100

    chg and del
    chg: svg position
    del: not used icons

--- src/components/Card/completedTask/CompletedTask.js
+++ src/components/Card/completedTask/CompletedTask.js
@@ -1,19 +1,16 @@
-import styles from './CompletedTask.module.css';
-import { ReactComponent as Arrow } from '../../../assests/completedTask/arrow.svg';
-import { ReactComponent as Platform } from '../../../assests/completedTask/platform.svg';
 import { ReactComponent as Shield } from '../../../assests/completedTask/shield.svg';
-import { ReactComponent as Dart } from '../../../assests/completedTask/dart.svg';
+import { ReactComponent as Platform } from '../../../assests/completedTask/platform.svg';
+import { ReactComponent as Arrow } from '../../../assests/completedTask/arrow.svg';
 import { ReactComponent as Clouds } from '../../../assests/completedTask/clouds.svg';
+import css from './CompletedTask.module.css';
 
 export const CompletedTask = () => {
 	return (
-		<div className={styles.completeWrap}>
-			<div className={styles.award}>
-				<Shield className={styles.shield}></Shield>
-				<Platform className={styles.platform}></Platform>
-				<Dart className={styles.dart}></Dart>
-				<Clouds className={styles.clouds}></Clouds>
-			</div>
+		<div className={css.completedTaskWrapper}>
+			<Shield className={css.shield}></Shield>
+			<Platform className={css.platform}></Platform>
+			<Arrow className={css.arrow}></Arrow>
+			<Clouds className={css.clouds}></Clouds>
 		</div>
 	);
 };
