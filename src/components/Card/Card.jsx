878b508f743d43798171cf0edc5ecffc85517fba
Michał Jarek <miciu77@gmail.com>
2023-02-27 19:48:40 +0100

    minBugfix

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,66 +1,74 @@
-import React, { useState } from 'react';
-import ReactCardFlip from 'react-card-flip';
-import { ReactComponent as FireIcon } from './images/fire.svg';
-import { ReactComponent as StarIcon } from './images/star.svg';
-import { ReactComponent as TrophyIcon } from './images/trophy.svg';
-import { ReactComponent as QuestAwardIcon } from './images/award.svg';
-import { ReactComponent as ChallengeAwardIcon } from './images/challenge-award.svg';
-import { ReactComponent as ArrowIcon } from './images/arrow.svg';
-import { CardEdition } from '../CardEdition/CardEdition';
-import DeleteModal from '../DeleteModal/DeleteModal';
-import { useCompleteCardMutation } from '../../redux/auth/authOperations';
-import { useTimeout } from './helperFunctions/dateAndTime/timeout';
-import { convertDayDisplay } from './helperFunctions/dateAndTime/dayConverter';
+import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
+import { ReactComponent as FireIcon } from "./images/fire.svg";
+import { ReactComponent as StarIcon } from "./images/star.svg";
+import { ReactComponent as TrophyIcon } from "./images/trophy.svg";
+import { ReactComponent as ChallengeAwardIcon } from "./images/challenge-award.svg";
+import { ReactComponent as ArrowIcon } from "./images/arrow.svg";
+import { CardEdition } from "../CardEdition/CardEdition";
+import DeleteModal from "../DeleteModal/DeleteModal";
+import { useCompleteCardMutation } from "../../redux/auth/authOperations";
+import { useTimeout } from "./helperFunctions/dateAndTime/timeout";
+import { convertDayDisplay } from "./helperFunctions/dateAndTime/dayConverter";
 import {
-	CardItem,
-	CardType,
-	Category,
-	DatetimeBar,
-	DifficultyBar,
-	FlippedCard,
-	ContinueBox,
-	CardContainer,
-	TitleDataWrapper,
-} from './Card.styled';
-import { CompletedTask } from './completedTask/CompletedTask';
+  CardItem,
+  CardType,
+  Category,
+  DatetimeBar,
+  DifficultyBar,
+  FlippedCard,
+  ContinueBox,
+  CardContainer,
+  TitleDataWrapper,
+} from "./Card.styled";
+import { CompletedTask } from "./completedTask/CompletedTask";
 
-export const Card = ({ id, title, difficulty, category, date, time, type, status }) => {
+export const Card = ({
+  id,
+  title,
+  difficulty,
+  category,
+  date,
+  time,
+  type,
+  status,
+}) => {
   const [isFlipped, setIsFlipped] = useState(false);
-  
+
   const [completeCard] = useCompleteCardMutation();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const toggleModal = () => setIsModalOpen((isModalOpen) => !isModalOpen)
+  const toggleModal = () => setIsModalOpen((isModalOpen) => !isModalOpen);
 
-	const toggleIsFlipped = () => {
-		if (status === 'Complete') {
-			return;
-		}
-		setIsFlipped(current => !current);
-	};
+  const toggleIsFlipped = () => {
+    if (status === "Complete") {
+      return;
+    }
+    setIsFlipped((current) => !current);
+  };
 
-	const convertedDate = convertDayDisplay(date, type);
+  const convertedDate = convertDayDisplay(date, type);
   const questTime = new Date(`${date}T${time}`).getTime();
   const timeout = useTimeout(questTime);
 
-	const isChallenge = type => type === 'Challenge';
-	const typeIcon = isChallenge(type) ? (
-		<TrophyIcon onClick={toggleIsFlipped} />
-	) : (
-		<StarIcon onClick={toggleIsFlipped} />
+  const isChallenge = (type) => type === "Challenge";
+  const typeIcon = isChallenge(type) ? (
+    <TrophyIcon onClick={toggleIsFlipped} />
+  ) : (
+    <StarIcon onClick={toggleIsFlipped} />
   );
-  
-	let awardIcon = null;
-	if (isFlipped) {
-		awardIcon = isChallenge(type) ? <ChallengeAwardIcon /> : <CompletedTask />;
-	}
+
+  let awardIcon = null;
+  if (isFlipped) {
+    awardIcon = isChallenge(type) ? <ChallengeAwardIcon /> : <CompletedTask />;
+  }
 
   const shortenedTitle = (() => {
     if (title.length > 18) {
       return `${title.slice(0, 17)}...`;
     } else if (undefined) {
-      return console.log("ERROR")
-    };
+      return console.log("ERROR");
+    }
     return title;
   })();
 
@@ -70,15 +78,9 @@ export const Card = ({ id, title, difficulty, category, date, time, type, status
 
   return (
     <CardContainer cardType={type}>
-      <ReactCardFlip
-        isFlipped={isFlipped}
-        flipDirection="horizontal"
-      >
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
         <CardItem cardType={type}>
-          <DifficultyBar
-            cardType={type}
-            difficulty={difficulty}
-          >
+          <DifficultyBar cardType={type} difficulty={difficulty}>
             <p>{difficulty}</p>
             {typeIcon}
           </DifficultyBar>
@@ -105,8 +107,8 @@ export const Card = ({ id, title, difficulty, category, date, time, type, status
           </ContinueBox>
           <DeleteModal
             isOpen={isModalOpen}
-            modalContent='Are you sure to mark card as done?'
-            nameOfConfirm='Yes'
+            modalContent="Are you sure to mark card as done?"
+            nameOfConfirm="Yes"
             cancelAction={toggleModal}
             confirmAction={() => completeCard(id)}
           />
@@ -124,7 +126,7 @@ export const Card = ({ id, title, difficulty, category, date, time, type, status
           cardCategory={category}
           onCancel={editClose}
         />
-     )} 
+      )}
     </CardContainer>
   );
 };
