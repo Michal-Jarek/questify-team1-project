c86a915a8292ef4dc3cf59b2cdaee166660c8b3b
Paweł Kłodowski <angryface97@gmail.com>
2023-03-01 10:36:24 +0100

    Add logic to developers list

--- src/components/AppBar/DevelopersList/DevelopersListBtn.js
+++ src/components/AppBar/DevelopersList/DevelopersListBtn.js
@@ -1,15 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import icons from "../../../assests/icons/sprite.svg";
 import { DevelopersBtn, DevelopersSvg } from "./DevelopersListBtn.styled";
+import { DevelopersListWrapper, DeleteMenu, CloseBtn, SvgClose } from "./DevelopersListBtn.styled";
 
-export const DevelopersListBtn = ({openModal}) => {
-  return (
+export const DevelopersListBtn = () => {
+ 
+  const [isOpen, setIsOpen] = useState(false)
+
+
+
+
+  return(
     <div title="Developers List">
-      <DevelopersBtn type="button" onClick={openModal}>
+      <DevelopersBtn type="button" onClick={() => setIsOpen(!isOpen)}>
         <DevelopersSvg width="46" height="46">
           <use xlinkHref={`${icons}#people`} />
         </DevelopersSvg>
       </DevelopersBtn>
+      <DevelopersListWrapper style={{display: isOpen ? "flex" : "none"}}>
+      <DeleteMenu>
+      <CloseBtn type='button' onClick={() => setIsOpen(!isOpen)}>
+					<SvgClose width='36' height='36'>
+						<use xlinkHref={`${icons}#close-button`} />
+					</SvgClose>
+				</CloseBtn>
+        <p>Developers List</p>
+        <div>
+        <ul>
+          <li>Michał Jarek</li>
+          <li>Dominika Sosnowska</li>
+          <li>Daria Ośka</li>
+          <li>Piotr Zdebski</li>
+          <li>Kalus Marek</li>
+          <li>Kłodowski Paweł</li>
+        </ul>
+        </div>
+      
+      </DeleteMenu>
+    </DevelopersListWrapper>
     </div>
   );
 };
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,14 +2,12 @@ import { AppBar } from 'components/AppBar/AppBar';
 import NewTaskBtn from 'components/NewTaskBtn/NewTaskBtn';
 // import { QuestList } from '../../components/QuestList/QuestList';
 import { CardGroupContainer } from 'components/CardGroup/CardGroupContainer';
-import DevelopersListModal from 'components/DevelopersList/DevelopersList';
 
 const Dashboard = () => {
     return (
     <>
         <AppBar />
         <CardGroupContainer />
-        <DevelopersListModal />
         {/* <QuestList /> */}
         <NewTaskBtn />
     </>
