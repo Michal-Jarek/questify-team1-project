64e486cb06208fca9ec5e28cce1eb1f4cc6032a8
Daria Ośka-Ossowska <daria.oska@gmail.com>
2023-03-01 23:32:23 +0100

    fix today and tommorow display

--- src/components/CardGroup/CardGroup.jsx
+++ src/components/CardGroup/CardGroup.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { QuestList } from '../QuestList/QuestList';
-// import { ReactComponent as ListUp } from './images/btnUp.svg';
-// import { ReactComponent as ListDown } from './images/btnDown.svg';
 import { todayDateInMs, tomorrowDateInMs } from '../Card/helperFunctions/dateAndTime/time';
 import { Div, GroupTitle } from './CardGroup.styled';
 
 export const CardGroup = ({ cards, groupName, hideLabel }) => {
 	const challengeState = useSelector(state => state.user.challengeFIlter);
 
-	const today = date => date === todayDateInMs;
-	const tomorrow = date => date === tomorrowDateInMs;
-
 	const assignCard = cardData => {
 		const { date, status } = cardData;
 		const checkedDate = new Date(date).getTime();
+		const dateWithoutTime = new Date(date).setHours(0, 0, 0, 0);
+		const today = new Date().setHours(0, 0, 0, 0);
+		const tomorrow = new Date(new Date().setDate(new Date().getDate() + 1)).setHours(0, 0, 0, 0);
 
 		if (status === 'complete') return 'done';
 		if (checkedDate < todayDateInMs) return 'past';
-		if (today(checkedDate)) return 'today';
-		if (tomorrow(checkedDate)) return 'tomorrow';
+		if (today === dateWithoutTime) return 'today';
+		if (tomorrow === dateWithoutTime) return 'tomorrow';
 		if (checkedDate > tomorrowDateInMs) return 'upcoming';
 	};
 
