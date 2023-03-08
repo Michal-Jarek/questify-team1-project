25d3fa656f815078129f7fd00bd6f20da3880bf4
Michał Jarek <miciu77@gmail.com>
2023-03-08 12:18:59 +0100

    Dla esLinta ;)

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,7 @@
 import "./App.css";
 import Dashboard from "pages/Dashboard/Dashboard";
-import { returnAllCards } from "redux/auth/cardsSelectors";
-import { useSelector } from "react-redux";
 
 const App = () => {
-
   return (
     <div className="App">
       <Dashboard />
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,12 @@
 import { AppBar } from "components/AppBar/AppBar";
 import NewTaskBtn from "components/NewTaskBtn/NewTaskBtn";
-// import { QuestList } from '../../components/QuestList/QuestList';
 import { CardGroupContainer } from "components/CardGroup/CardGroupContainer";
-import { useSelector } from "react-redux";
-import { appState } from "redux/auth/cardsSelectors";
 
 const Dashboard = () => {
   return (
     <>
       <AppBar />
       <CardGroupContainer />
-      {/* <QuestList /> */}
       <NewTaskBtn />
     </>
   );
